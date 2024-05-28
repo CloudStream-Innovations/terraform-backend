aa934a86378668eaf1f35f10cdefa69be32f50f1
Warren Byron <wbyron@shoprite.co.za>
2024-05-28 17:07:41 +0100

    Added backend variable

--- main.tf
+++ main.tf
@@ -13,8 +13,12 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+variable "backend" {
+  type = string
+}
+
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state-dcum2iwvkp"
+  bucket = "terraform-state-${var.backend}"
 
   # When a resource has *prevent_destroy* set to true, Terraform will exit with an error if any effort is made to 
   # delete it (for example, by running terraform destruction). An critical resource, like this S3 bucket, which will 
@@ -66,7 +70,7 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
 # Create a DynamoDB table with the primary key LockID to be used with Terraform as a locking mechanism.
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-state-dcum2iwvkp"
+  name         = "terraform-state-${var.backend}"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
